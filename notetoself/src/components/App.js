bbe3dd386e4d487cbdd5701ecde58b67264b0e40
David Katz <[INSERT EMAIL]>
2017-08-07 07:39:51 -0700

    Update Form with state

--- notetoself/src/components/App.js
+++ notetoself/src/components/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      text: ''
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Note to Self</h2>
-        <Form>
-          <FormControl />
-          <Button>Submit</Button>
+        <Form inline>
+          <FormControl onChange={event => this.setState({ text: event.target.value }) } />
+          {' '}
+          <Button onClick={() => console.log(this.state)}>Submit</Button>
         </Form>
       </div>
     )
