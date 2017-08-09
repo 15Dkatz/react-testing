f44b5fc469047d1304d45b233b6c1efdb8c6c6a4
David Katz <[INSERT EMAIL]>
2017-08-09 07:37:13 -0700

    Add flashcardpro

---
+++ flashcardpro/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Flashcard Pro</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
