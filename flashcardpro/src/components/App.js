bd7fd85545793bd62404aa9e532ff608a93e8829
David Katz <[INSERT EMAIL]>
2017-08-12 18:25:09 -0700

    Convert Stack and App to functional stateless components

--- flashcardpro/src/components/App.js
+++ flashcardpro/src/components/App.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import StackList from './StackList';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Flashcard Pro</h2>
-        <hr/>
-        <StackList />
-        <hr />
-        <Link to='stack_form'><h4>Create a New Stack</h4></Link>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      <h2>Flashcard Pro</h2>
+      <hr/>
+      <StackList />
+      <hr />
+      <Link to='stack_form'><h4>Create a New Stack</h4></Link>
+    </div>
+  )
 }
 
 export default App;
--- flashcardpro/src/components/Stack.js
+++ flashcardpro/src/components/Stack.js
@@ -3,27 +3,23 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Card from './Card';
 
-export class Stack extends Component {
-  render() {
-    const { title, cards } = this.props.stack;
-
-    return (
-      <div>
-        <Link className='link-home' to='/'>
-          <h4>Home</h4>
-        </Link>
-        <h3>{title}</h3>
-        <br />
-        {
-          cards.map(card => {
-            return (
-              <Card key={card.id} card={card} />
-            )
-          })
-        }
-      </div>
-    )
-  }
+export const Stack = ({ stack: { title, cards } }) => {
+  return (
+    <div>
+      <Link className='link-home' to='/'>
+        <h4>Home</h4>
+      </Link>
+      <h3>{title}</h3>
+      <br />
+      {
+        cards.map(card => {
+          return (
+            <Card key={card.id} card={card} />
+          )
+        })
+      }
+    </div>
+  )
 }
 
 function mapStateToProps(state) {
